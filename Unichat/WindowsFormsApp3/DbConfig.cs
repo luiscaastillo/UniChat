5e55b3e4cd89fc8fbe40637f7c741abf38531dfd
Luis Castillo <luiseduardocastilloruvalcaba@gmail.com>
2025-10-06 18:08:15 -0600

    hashing passwords implemented2

---
+++ Unichat/WindowsFormsApp3/DbConfig.cs
@@ -0,0 +1,7 @@
+namespace Unichat
+{
+    public static class DbConfig
+    {
+        public static readonly string connectionString = "server=127.0.0.1;uid=root;pwd=rootroot;database=unichat";
+    }
+}
