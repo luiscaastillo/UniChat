c3d9837c18b2d7c3b5113e913d50bc8d07d12a21
Luis Castillo <luiseduardocastilloruvalcaba@gmail.com>
2025-10-06 18:07:29 -0600

    hashing passwords implemented

--- Unichat/WindowsFormsApp3/FormRegister.Designer.cs
+++ Unichat/WindowsFormsApp3/FormRegister.Designer.cs
@@ -31,12 +31,12 @@
             this.labelContra = new System.Windows.Forms.Label();
             this.labelUsuario = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.Bconectar = new System.Windows.Forms.PictureBox();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
             this.textBoxContra = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBoxUsuario = new System.Windows.Forms.TextBox();
-            this.Bconectar = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Bconectar)).BeginInit();
             this.SuspendLayout();
@@ -44,20 +44,22 @@
             // labelContra
             // 
             this.labelContra.AutoSize = true;
-            this.labelContra.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelContra.Location = new System.Drawing.Point(146, 177);
+            this.labelContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContra.Location = new System.Drawing.Point(292, 340);
+            this.labelContra.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelContra.Name = "labelContra";
-            this.labelContra.Size = new System.Drawing.Size(150, 23);
+            this.labelContra.Size = new System.Drawing.Size(245, 48);
             this.labelContra.TabIndex = 13;
             this.labelContra.Text = "Contraseña";
             // 
             // labelUsuario
             // 
             this.labelUsuario.AutoSize = true;
-            this.labelUsuario.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelUsuario.Location = new System.Drawing.Point(146, 109);
+            this.labelUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsuario.Location = new System.Drawing.Point(292, 210);
+            this.labelUsuario.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelUsuario.Name = "labelUsuario";
-            this.labelUsuario.Size = new System.Drawing.Size(108, 23);
+            this.labelUsuario.Size = new System.Drawing.Size(171, 48);
             this.labelUsuario.TabIndex = 11;
             this.labelUsuario.Text = "Usuario";
             this.labelUsuario.Click += new System.EventHandler(this.label2_Click);
@@ -70,27 +72,50 @@
             this.panel1.Controls.Add(this.linkLabel1);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(94, 23);
+            this.panel1.Location = new System.Drawing.Point(188, 44);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(425, 317);
+            this.panel1.Size = new System.Drawing.Size(850, 610);
             this.panel1.TabIndex = 17;
+            // 
+            // Bconectar
+            // 
+            this.Bconectar.Location = new System.Drawing.Point(264, 398);
+            this.Bconectar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Bconectar.Name = "Bconectar";
+            this.Bconectar.Size = new System.Drawing.Size(262, 110);
+            this.Bconectar.TabIndex = 15;
+            this.Bconectar.TabStop = false;
+            this.Bconectar.Click += new System.EventHandler(this.Bconectar_Click_1);
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(114, 217);
+            this.textBoxUsuario.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(556, 31);
+            this.textBoxUsuario.TabIndex = 14;
+            this.textBoxUsuario.Text = "Ingresa el nombre de usuario";
             // 
             // textBoxContra
             // 
-            this.textBoxContra.Location = new System.Drawing.Point(57, 181);
+            this.textBoxContra.Location = new System.Drawing.Point(114, 348);
+            this.textBoxContra.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxContra.Name = "textBoxContra";
-            this.textBoxContra.Size = new System.Drawing.Size(280, 20);
+            this.textBoxContra.Size = new System.Drawing.Size(556, 31);
             this.textBoxContra.TabIndex = 13;
+            this.textBoxContra.Text = "Ingresa la contraseña";
             this.textBoxContra.TextChanged += new System.EventHandler(this.textBoxContra_TextChanged);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(197)))), ((int)(((byte)(255)))));
-            this.linkLabel1.Location = new System.Drawing.Point(225, 267);
+            this.linkLabel1.Location = new System.Drawing.Point(450, 513);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(79, 16);
+            this.linkLabel1.Size = new System.Drawing.Size(159, 29);
             this.linkLabel1.TabIndex = 12;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Inicia sesión";
@@ -99,10 +124,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(96, 267);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(192, 513);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 15);
+            this.label4.Size = new System.Drawing.Size(263, 26);
             this.label4.TabIndex = 11;
             this.label4.Text = "¿Ya tienes una cuenta?";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -110,38 +136,24 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("OCR A Extended", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Location = new System.Drawing.Point(36, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(387, 29);
+            this.label1.Size = new System.Drawing.Size(580, 63);
             this.label1.TabIndex = 2;
             this.label1.Text = "¡Registrate a UniChat!";
             // 
-            // textBoxUsuario
-            // 
-            this.textBoxUsuario.Location = new System.Drawing.Point(57, 113);
-            this.textBoxUsuario.Name = "textBoxUsuario";
-            this.textBoxUsuario.Size = new System.Drawing.Size(280, 20);
-            this.textBoxUsuario.TabIndex = 14;
-            // 
-            // Bconectar
-            // 
-            this.Bconectar.Location = new System.Drawing.Point(132, 207);
-            this.Bconectar.Name = "Bconectar";
-            this.Bconectar.Size = new System.Drawing.Size(131, 57);
-            this.Bconectar.TabIndex = 15;
-            this.Bconectar.TabStop = false;
-            this.Bconectar.Click += new System.EventHandler(this.Bconectar_Click_1);
-            // 
             // FormRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 362);
+            this.ClientSize = new System.Drawing.Size(1224, 696);
             this.Controls.Add(this.labelContra);
             this.Controls.Add(this.labelUsuario);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "FormRegister";
             this.Text = "UniChat Registro";
             this.Load += new System.EventHandler(this.FormRegister_Load);
